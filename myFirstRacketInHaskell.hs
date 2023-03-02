dcb700e8d477e83a3e44d9062b035585113e1e05
Stephen Aranda <aranda.stephen88@gmail.com>
2023-03-02 13:05:37 -0600

    added author comment for function 9

--- myFirstRacketInHaskell.hs
+++ myFirstRacketInHaskell.hs
@@ -5,11 +5,20 @@ module Main (main) where
 -- Date  : 3/1/23
 -- Desc  : This is the haskell versions of my first Racket Programming project.
 
+-----------divisibleByX?: Callback function that is given two values and
+----------- determines if param y is evenly divisible by param x
+
 yDivByX :: (Integer -> Integer -> Bool) -> Integer -> Bool -- Function declaration for func that will call divisibleByX
 yDivByX func y = func y 5 -- Function definition for func that will call divisibleByX
 
 divisibleByX :: Integer -> Integer -> Bool -- Function declaration for divisibleByX
 divisibleByX y x = (y `mod` x) == 0 -- function definition for divisibleByX
+
+--------------------------------------------------------------------------------
+
+------------function-9: Higher order function that takes a function
+------------as an argument and that function takes an integer(specifically 9) and applies it
+------------to some operation occurring within that function
 
 -- Entry point for test cases of all functions that will be demonstrated
 main :: IO ()
